b93e3db5fd1a762f99814d326b6da0d4da6cdaa6
Zeynab Jalalian <zjalalianweb@gmail.com>
2025-07-02 14:56:47 +0330

    js

---
+++ script.js
@@ -0,0 +1,28 @@
+const search = document.querySelector(".search-box input"),
+    images = document.querySelectorAll(".image-box");
+
+
+search.addEventListener("keyup", e => {
+    if (e.key == "Enter") {
+        let searchValue = search.value,
+            value = searchValue.toLowerCase();
+
+        images.forEach(image => {
+            if (value === image.dataset.name.toLowerCase()) {
+                return image.style.display = "block";
+            } else {
+                return image.style.display = "none";
+            }
+        })
+    }
+
+
+})
+
+search.addEventListener("keyup",()=>{
+    if(search.value !== "") return;
+
+    images.forEach(image=>{
+         return image.style.display = "block";
+    })
+})
